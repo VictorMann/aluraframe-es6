ef407cd62f379c2df0571008d1704a78d12aa1f3
Victor Mann <victorcmann@gmail.com>
2018-11-12 20:36:57 -0200

    criando NegociacaoController e definido contructor

---
+++ client/js/app/controllers/NegociacaoController.js
@@ -0,0 +1,11 @@
+class NegociacaoController
+{
+    constructor()
+    {
+        let $ = document.querySelector.bind(document);
+    
+        this.inputData = $('#data');
+        this.inputQuantidade = $('#quantidade');
+        this.inputValor = $('#valor');
+    }
+}

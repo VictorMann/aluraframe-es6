ded30a5bedeb1c7bc71f75206e80dc8bc2d6965d
victormann <victorcmann@gmail.com>
2018-11-17 12:15:50 -0200

    criando Bind para realizacao do data biding entre model e view

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -8,23 +8,19 @@ class NegociacaoController
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
 
-        let self = this;
-        
         this._negociacoesView = new NegociacoesView($('#negociacoesView'));
-        this._listaNegociacoes = ProxyFactory.create(
-            new ListaNegociacoes, 
-            ['adiciona', 'esvazia'],
-            model => this._negociacoesView.update(model)
+        this._listaNegociacoes = new Bind(
+            new ListaNegociacoes,
+            this._negociacoesView,
+            'adiciona', 'esvazia'
         );
-        this._negociacoesView.update(this._listaNegociacoes);
-
+        
         this._mensagemView = new MensagemView($('#mensagemView'));
-        this._mensagem = ProxyFactory.create(
+        this._mensagem = new Bind(
             new Mensagem,
-            ['texto'],
-            model => this._mensagemView.update(model)
+            this._mensagemView,
+            'texto'
         );
-        this._mensagemView.update(this._mensagem);
     }
 
     adiciona(event)

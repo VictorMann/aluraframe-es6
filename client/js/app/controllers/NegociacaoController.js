ad6b5e6b877e5de9a6d68ed6d917faed7a8bd16e
Victor Mann <victorcmann@gmail.com>
2018-11-20 10:51:17 -0200

    Listando todas as negociacoes ao carregar a pagina

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -21,6 +21,15 @@ class NegociacaoController
             this._mensagemView,
             'texto'
         );
+
+        ConnectionFactory
+        .getConnection()
+        .then(connection => new NegociacaoDao(connection))
+        .then(dao => dao.listaTodos())
+        .then(negociacoes => 
+            negociacoes.forEach(negociacao => 
+                this._listaNegociacoes.adiciona(negociacao))
+        );
     }
 
     adiciona(event)
